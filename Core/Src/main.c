08153413dc7ed198a4fb588ff72ffa46b3dc26e2
Johnathan Christian Leys <johnathan.leys@maine.edu>
2023-02-10 16:56:35 -0500

    User Space Shell

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -34,7 +34,7 @@ Modidied main, dependancaies and original code from STMicroelectronics.
 #include "Adafruit_ST7735.h"
 #include "graphics.h"
 #include "progs.h"
-
+#include "sh.h"
 
 
 /* USER CODE END Includes */
@@ -62,8 +62,7 @@ Modidied main, dependancaies and original code from STMicroelectronics.
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-int sh(void); //single threaded process shell
-int sh_getline(char* inputLine);
+
 
 /* USER CODE END PFP */
 
@@ -133,9 +132,9 @@ printf("start\n\r");
   {
     /* USER CODE END WHILE */
    
-   //sh();
+   sh();
    //process1();
-   process2();
+   //process2();
 
 
 
@@ -144,68 +143,7 @@ printf("start\n\r");
   /* USER CODE END 3 */
   return 0;
 }//end Main
-//Project 01B code
-int sh(void){
-char inputLine[100]; //initial declaration
 
-
-sh_getline(inputLine );
-
-printf("%s\n\r", inputLine);
-
-if((strncmp(inputLine, "echo ", 5)) == 0){ //if first 5 chars are "echo " with a space
-    printf("%s\n\r", inputLine + 5);
-}
-
-
-return 0;
-}
-/*
-"Use getchar() to get a single character. Manage the string buffer such that a long line of input does not
-beyond the end of the input line string. Return if a newline or carriage return is detected. Appropriately handle
-backspace"
-*/
-
-int sh_getline(char *inputLine){
-
-char holder; //hold each char that is read in
-
-int iter =0; //iterator
-holder = getchar(); //get initial char holder = getchar();
-putchar(holder);
-
-while(holder != '\n' && holder != '\r'){ //loop while holder char is not newline or carriage return  
-   
-    
-    if((holder != '\b' )&&(iter <=100)){ //if not backspace, not full string
-        inputLine[iter] = holder; //set input char to array value[iter]
-        iter++;
-    }
-    else if (iter > 0){//make sure we dont run into negative space
-        iter--; //decrement iter to replace previous char
-        printf(" \b"); // replace with " ", go back, appears to make blank
-           
-    }
- 
-   
-
-    /*
-    if(iter >= 100){ //try to handle bigger lines
-    char *tempArray = (char *)calloc(iter + 10, sizeof(char)); //is calloc allowed?
-    strcpy(tempArray, inputLine); //set them to be same
-    inputLine=tempArray;//set to new array (larger)
-    }*/
-    
-
-    holder = getchar();//grab new char for next iteration
-   printf("%c", holder);//show characters on screen like bash, can handle oo length
-    
-    
-}
-
-inputLine[iter] = '\0'; //string termination
-return 0;//successful completion
-}
 
 
 
