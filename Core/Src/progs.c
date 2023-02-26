678472bcec50a0c934b595d65e9927c876b8f459
Johnathan Christian Leys <johnathan.leys@maine.edu>
2023-02-26 16:25:54 -0500

    replace hal_delay

--- Core/Src/progs.c
+++ Core/Src/progs.c
@@ -1,12 +1,13 @@
 #include "progs.h"
 #include "main.h"
+#include "user_syscalls.h"
 
 int process1(void)
 {
 	while (1) {
-		HAL_Delay(250);	//delay to get 2hz, 4 toggles/sec)
+		microsleep(250000);
 		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);	//toggle green LED
-		HAL_Delay(250);
+		microsleep(250000);
 		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
 	}
     return 1;
@@ -15,9 +16,9 @@ int process1(void)
 void process2(void)
 {
 	while (1) {
-		HAL_Delay(500);	//delay to get 1hz, 2 toggles/sec)
+		microsleep(500000);	//delay to get 1hz, 2 toggles/sec)
 		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);	//toggle amber LED
-		HAL_Delay(500);
+		microsleep(500000);
 		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 	}
 }
--- Core/Src/user_syscalls.c
+++ Core/Src/user_syscalls.c
@@ -3,9 +3,9 @@
 
 
 uint32_t microsleep(uint32_t sleep_time){
-    current->w_time = sleep_time + (uwTick * 1000); //set wake time based on sleep time
+    current->w_time = sleep_time + (uwTick * 1000); //set wake time based on sleep time, change uwTick to micro
     current->state = current->state | time_sleep; //change state to sleep, 0x01
 
-    return 1; //tmp
+    return 1; //tmp "The function returns the amount of time left to sleep (ignore this for now)"
 
 }
